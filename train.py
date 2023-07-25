27b488de4c7a4a931337d4c641326d6f4029621d
HugoAlbuquerque-ABI <Hugo.Albuquerque@interbrew.net>
2023-07-24 21:48:00 -0300

    adding project files

--- train.py
+++ train.py
@@ -54,18 +54,23 @@ def main():
     # TODO: Create TabularDataset using TabularDatasetFactory
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-    ds = ### YOUR CODE HERE ###
+    
+    ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###a
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=42)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    
+    # save model as run output
+    os.makedirs('outputs',exist_ok = True)
+    joblib.dump(model,'outputs/model.joblib')
+
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
